43524e65cf0505a1b7a1f78702023d2402c45d01
viktormoskalev07 <viktormoskalev07@gmail.com>
2021-06-13 17:07:56 +0300

    mailer and insta

--- src/js/fetch-inst.js
+++ src/js/fetch-inst.js
@@ -1,36 +1,26 @@
 function instagramIntegration() {
   let xhr = new XMLHttpRequest();
-  let container = document.querySelector("#instagam-cont");
+  const container = document.querySelector("#instagam-cont");
+  const instaUserName = document.querySelectorAll(".instagram__user-name"); 
+  const instaImg = document.querySelectorAll(".instagram__image img");
+  const instaOld = document.querySelectorAll(".instagram__img__old");
 
   // put here your instagram feed id from curator io
   //const INST_FEED_ID = '16ffe87b-cb7e-4091-8ff6-bd0b35f8ded6';
-  var instPost = container.getAttribute('data-instagram-post');
+  const instPost = container.getAttribute('data-instagram-post');
 
   xhr.onload = function() {
     if (xhr.status === 200) {
       let posts = JSON.parse(xhr.response).posts;
       if(posts){
-        for (let i = 0; i < 4; i++) {
-          let post = posts[i];
-          let postItem = document.createElement('div');
-          postItem.setAttribute('class', 'instagram__item');
-
-          // create/embed <img>
-          let imagePost = document.createElement('img');
-          let imageSrc = post.image;
-          imagePost.setAttribute('src', imageSrc);
-          imagePost.setAttribute('alt', 'instagram post image');
-
-          // create userName <p>
-          let userNameCaption = document.createElement('p');
-          let userName = post.user_full_name
-          userNameCaption.setAttribute('class', 'instagram__user-name')
-          userNameCaption.innerHTML = `@${userName}`
-
-          postItem.appendChild(imagePost);
-          postItem.appendChild(userNameCaption)
-  
-          container.appendChild(postItem)        
+        for (let i = 0; i < 4; i++) { 
+          try{
+            const post = posts[i];  
+            instaUserName[i].innerHTML = "@"+post.user_full_name;  
+            instaImg[i].src=post.image;   
+          }catch (e){
+            console.log(e);
+          } 
         }
       }
     }
